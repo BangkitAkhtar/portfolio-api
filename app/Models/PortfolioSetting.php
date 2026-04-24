f01f5ce976ca6f9be1cdcc5275dd0d87ba87dbfb
BangkitAkhtar <bangkitfahd@gmail.com>
2026-04-24 14:26:53 +0700

    update: ERROR SEKARANG

--- app/Models/PortfolioSetting.php
+++ app/Models/PortfolioSetting.php
@@ -6,8 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class PortfolioSetting extends Model
 {
+    // Pastikan kolom data diizinkan untuk diisi
     protected $fillable = ['data'];
 
+    // WAJIB ADA: Beri tahu Laravel untuk mengubah JSON menjadi Array secara otomatis
     protected $casts = [
         'data' => 'array',
     ];
